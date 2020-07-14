26cf483e20c695c417f5dcad7dc87ea5ec0b9648
Carlos Ezeta A <52421331+ezeta506@users.noreply.github.com>
2020-07-13 22:23:09 -0600

    mostrar datos pelicula en angular

--- src/app/pelicula/pelicula-routing.module.ts
+++ src/app/pelicula/pelicula-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PeliculaIndexComponent } from './pelicula-index/pelicula-index.component';
+import { PeliculaShowComponent } from './pelicula-show/pelicula-show.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pelicula', component: PeliculaIndexComponent },
+  { path: 'pelicula/:id', component: PeliculaShowComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pelicula/pelicula.module.ts
+++ src/app/pelicula/pelicula.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PeliculaRoutingModule } from './pelicula-routing.module';
+import { PeliculaIndexComponent } from './pelicula-index/pelicula-index.component';
+import { PeliculaShowComponent } from './pelicula-show/pelicula-show.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PeliculaIndexComponent, PeliculaShowComponent],
   imports: [
     CommonModule,
     PeliculaRoutingModule
