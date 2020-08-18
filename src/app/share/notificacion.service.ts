214103d6ee9de69ae7971937c0620dc7bf2a2e78
Carlos Ezeta A <52421331+ezeta506@users.noreply.github.com>
2020-08-17 18:29:53 -0600

    semana8

--- src/app/share/notificacion.service.ts
+++ src/app/share/notificacion.service.ts
@@ -8,7 +8,8 @@ export class NotificacionService {
     /* Top Right, Bottom Right, Bottom Left, Top Left, Top Full Width, Bottom Full Width, Top Center, Bottom Center */
     this.options.positionClass = 'toast-top-full-width';
     //Tiempo que se presenta el mensaje
-    this.options.timeOut = 5000;
+    this.options.disableTimeOut = true;
+    this.options.closeButton= true;
     //Habilitar formato HTML dentro de la notificación
     this.options.enableHtml = true;
   }
@@ -16,21 +17,26 @@ export class NotificacionService {
   public mensaje(titulo: string, mensaje: string, tipo: string) {
     this.toastr.show(mensaje, titulo, this.options, 'toast-' + tipo);
   }
-  /* Gestión de mensaje de validación de formularios para presentarlos en una notificación */ msjValidacion(
-    errores: any
-  ) {
+  /* Gestión de mensaje de validación de formularios para presentarlos en una notificación */
+  msjValidacion(errores: any) {
     let mensaje = '';
     if (errores != null) {
       if (errores.error.errors) {
         for (const item of errores.error.errors) {
           mensaje += item.message + ' <br />';
         }
+      }else{
+        if(errores.error.error){
+          mensaje += errores.error.error;
+        }
       }
-      this.mensaje(mensaje, errores.error.message, 'warning ');
+
     } else {
       if (errores.error) {
         mensaje += errores.error + ' <br />';
       }
     }
+   // this.mensaje(mensaje, errores.error.message, 'warning ');
+    this.toastr.show('', mensaje, this.options,'toast-warning');
   }
 }
